75890a7bce4e442b07f7e1e69f3940eb1cfd32ad
SviatoslavSiatynia <150059661+SviatoslavSiatynia@users.noreply.github.com>
2023-12-15 13:53:34 +0200

    Week-4 Homework (#4)
    * Week-4 Homework
    
    * week-4 homework fixes with pattern matching and other
    
    * week-4 homework fix def remove
    
    * week-4 homework delete def maxValueAndRemove

--- src/main/scala/karazin/scala/users/group/week4/homework/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week4/homework/Homework.scala
@@ -35,23 +35,27 @@ object Homework:
 
     infix def contains(x: Int): Boolean = false
 
-    infix def remove(x: Int): IntSet = ???
+    infix def remove(x: Int): IntSet = throw new Exception("You cannot remove an element from an Empty")
     
     @targetName("union")
-    infix def ∪(that: IntSet): IntSet = ???
+    infix def ∪(that: IntSet): IntSet = that
 
     @targetName("intersection")
-    infix def ∩(that: IntSet): IntSet = ???
+    infix def ∩(that: IntSet): IntSet = Empty
 
     @targetName("complement")
-    infix def ∖(that: IntSet): IntSet = ???
+    infix def ∖(that: IntSet): IntSet = Empty
 
     @targetName("disjunctive union")
-    infix def ∆(that: IntSet): IntSet = ???
+    infix def ∆(that: IntSet): IntSet = that
     
     override def toString: String = "[*]"    
     
-    override def equals(other: Any): Boolean = ???    
+    override def equals(other: Any): Boolean =
+      if other.isInstanceOf[Empty] then true
+      else false
+
+    override def hashCode(): Int = 0
 
   end Empty
     
@@ -68,23 +72,55 @@ object Homework:
       else              true
 
     // Optional task
-    infix def remove(x: Int): IntSet = ???
+    infix def remove(x: Int): IntSet =
+      if this contains x then
+        if x == elem then left ∪ right
+        else if x < elem then NonEmpty(elem, left.remove(x), right)
+        else NonEmpty(elem, left, right.remove(x))
+      else throw new Exception("The element is not included in NonEmpty")
 
     @targetName("union")
-    infix def ∪(that: IntSet): IntSet = ???
+    infix def ∪(that: IntSet): IntSet =
+      that match
+        case thatNonEmpty: NonEmpty =>
+          (this include thatNonEmpty.elem) ∪ thatNonEmpty.left ∪ thatNonEmpty.right
+        case _ => this
 
     @targetName("intersection")
-    infix def ∩(that: IntSet): IntSet = ???
+    infix def ∩(that: IntSet): IntSet =
+      that match
+        case thatNonEmpty: NonEmpty =>
+          if this contains thatNonEmpty.elem then NonEmpty(thatNonEmpty.elem, this ∩ thatNonEmpty.left, this ∩ thatNonEmpty.right)
+          else (this ∩ thatNonEmpty.left) ∪ (this ∩ thatNonEmpty.right)
+        case _ => Empty
 
     @targetName("complement")
-    infix def ∖(that: IntSet): IntSet = ???
+    infix def ∖(that: IntSet): IntSet =
+      that match
+        case thatNonEmpty: NonEmpty =>
+          if thatNonEmpty contains this.elem then (this.left ∖ thatNonEmpty) ∪ (this.right ∖ thatNonEmpty)
+          else NonEmpty(this.elem, this.left ∖ thatNonEmpty, this.right ∖ thatNonEmpty)
+        case _ => this
 
     @targetName("disjunctive union")
-    infix def ∆(that: IntSet): IntSet = ???
+    infix def ∆(that: IntSet): IntSet =
+      that match
+        case thatEmpty: Empty => this
+        case _ => (this ∖ that) ∪ (that ∖ this)
     
     override def toString: String = s"[$left - [$elem] - $right]"    
-    
-    override def equals(other: Any): Boolean = ???
+    override def equals(other: Any): Boolean =
+      if other.isInstanceOf[NonEmpty] then
+        val that = other.asInstanceOf[NonEmpty]
+        if this.hashCode != that.hashCode then false
+        else
+          (this.elem == that.elem) && (this.left == that.left) && (this.right == that.right)
+      else false
+
+    override def hashCode(): Int = {
+      val state = Seq(elem.hashCode, left.hashCode, right.hashCode)
+      state.map(_.hashCode()).foldLeft(0)((a, b) => 31 * a + b)
+    }
 
   end NonEmpty
 
